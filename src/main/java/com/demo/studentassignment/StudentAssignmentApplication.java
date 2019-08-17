adbf4a62e780fa74860d572932907a5b9bede6d9
vipinv1987 <vipin072@gmail.com>
2019-08-17 15:56:01 +0530

    Initial Commit

---
+++ src/main/java/com/demo/studentassignment/StudentAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.demo.studentassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAssignmentApplication.class, args);
+	}
+
+}
